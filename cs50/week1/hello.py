cd23ded409e610d783401f922cb72e0d81f07215
patientenvy <oluwatobi.adetula@greenpegltd.com>
2024-02-22 08:52:50 +0100

    build: use str type options

--- cs50/week1/hello.py
+++ cs50/week1/hello.py
@@ -4,7 +4,16 @@ Ask a user for their name, demonstrating the use of input and print functions
 
 """ 
  
-name = input("What's your name? ") # Print a string to Terminal and recieve user input
+firstName = input("What's your name? ") # Print a string to Terminal and recieve user input
+firstName = firstName.strip() # Strip the whitespace from string and store the altered version in the same variable
 
 # Print user name to terminal after storing in variable name 
-print("Hello,",name) # Print a String to Terminal 
+print("Hello,",firstName) # Print a String to Terminal
+
+# Add a seperate section asking for last name and demonstrating print function override
+lastName = input("and your last name? ")
+
+# Print function overrides using named Parameters
+print("Nice to meet you, ", firstName ,end ="")
+print(lastName)
+

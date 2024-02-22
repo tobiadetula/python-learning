467f6515bb7c5d6521ddde0f90d5ae2925b7ca11
patientenvy <oluwatobi.adetula@greenpegltd.com>
2024-02-22 09:54:43 +0100

    build: type conversion and mathetical operators

---
+++ cs50/week1/calculator.py
@@ -0,0 +1,13 @@
+"""_summary_
+    requests for user input as string, the converts to int 
+    and performs addition 
+"""
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+
+# Variable addition 
+# z = int(x) + int(y) // Alternate version
+z = x + y 
+print(z)

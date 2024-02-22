7028c6f19ea2d15ffdfa17320806bdba246b84b7
patientenvy <oluwatobi.adetula@greenpegltd.com>
2024-02-22 09:16:20 +0100

    build: function concatenation

--- cs50/week1/hello.py
+++ cs50/week1/hello.py
@@ -4,16 +4,27 @@ Ask a user for their name, demonstrating the use of input and print functions
 
 """ 
  
-firstName = input("What's your name? ") # Print a string to Terminal and recieve user input
-firstName = firstName.strip() # Strip the whitespace from string and store the altered version in the same variable
+# Print a string to Terminal and recieve user input
+name = input("What's your name? ")
+# Strip the whitespace from string and store the altered version in the same variable
+name = name.strip() 
+# Capitalize user's name 
+# name = name.capitalize() # only name before whitespace
+name = name.title()
+
 
 # Print user name to terminal after storing in variable name 
-print("Hello,",firstName) # Print a String to Terminal
+print("Hello,",name) # Print a String to Terminal
 
 # Add a seperate section asking for last name and demonstrating print function override
-lastName = input("and your last name? ")
+# Then remove whitespace from title
+# Then capitalize user's title
+
+title = input("and your title ").strip().capitalize()
+# title = title.strip() 
+# title = title.capitalize() 
 
 # Print function overrides using named Parameters
-print("Nice to meet you, ", firstName ,end ="")
-print(lastName)
+print("Nice to meet you, ",title,end =". ")
+print(name)
 

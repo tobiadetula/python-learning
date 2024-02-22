ecf3f90d07e80450bf251d61eb7c9014b65d5fc4
patientenvy <oluwatobi.adetula@greenpegltd.com>
2024-02-22 10:27:13 +0100

    build: output seperator

--- cs50/week1/calculator.py
+++ cs50/week1/calculator.py
@@ -2,12 +2,20 @@
     requests for user input as string, the converts to int 
     and performs addition 
 """
+# # Integer Definitions Demonstration
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
 
-x = int(input("What's x? "))
-y = int(input("What's y? "))
-
+# Float Definitions Demonstration
+x = float(input("What's x? "))
+y = float(input("What's y? "))
 
 # Variable addition 
 # z = int(x) + int(y) // Alternate version
-z = x + y 
-print(z)
+z = round(x + y,2) 
+
+# Printing output to terminal 
+# print(z) - Outputs sum of x and y, Ex: 20 + 980 = 1000
+
+# Outputs sum of x and y, Ex: 20 + 980 = 1,000 with seperator
+print(f"{z:,}")
